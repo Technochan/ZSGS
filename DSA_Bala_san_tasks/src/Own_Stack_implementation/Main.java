35a6e337bd958b9065e3895765e98e05f50a70fd
Technochan <mailtochan10@gmail.com>
2024-05-20 17:37:24 +0530

    DSA Sorting Algorithm Updated

--- DSA_Bala_san_tasks/src/Own_Stack_implementation/Main.java
+++ DSA_Bala_san_tasks/src/Own_Stack_implementation/Main.java
@@ -4,10 +4,7 @@ public class Main {
     public static void main(String[] args) {
         MyStack<Integer> queue = new MyStack<>();
         queue.push(10);
-//        queue.push(20);
-
         queue.printQueue();
-
         System.out.println(queue.isEmpty());
         System.out.println(queue.pop());
         System.out.println(queue.peek());
